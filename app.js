490943b015c9af57954c65b7c489e264cd12d45d
Till1983 <till_ermold@web.de>
2024-05-29 23:43:38 +0200

    CHANGE order of routes

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ import express from 'express'
 const app = express()
 const PORT = 3000
 
-app.listen(PORT , () => {
-    console.log(`Started server on ${PORT}`)
-})
-
 app.get('/', (request, response) => {
     response.send('Welcome to my 🍪 Cookieshop!')
   })
+
+app.listen(PORT , () => {
+    console.log(`Started server on ${PORT}`)
+})
