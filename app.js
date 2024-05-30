e35346d7d3214edacfc50addedc5e29a2ef84cdf
Till1983 <till_ermold@web.de>
2024-05-30 14:51:52 +0200

    ADD html to responses

--- app.js
+++ app.js
@@ -9,20 +9,20 @@ app.use('/assets', express.static('public'))
 app.use(express.urlencoded({extended: true}))
 
 app.get('/', (request, response) => {
-    response.send('Welcome to my 🍪 Cookieshop!')
+    response.send('<h1>Welcome!</h1><p>Welcome to my 🍪 Cookieshop!</p>')
   })
 
 app.get('/contact', (request, response) => {
-    response.send('Contact us if you have any questions!')
+    response.send('<h1>Get in touch</h1><p>Contact us if you have any questions!</p>')
 })
 
 app.post('/contact', (request, response) => {
     console.log('Contact form submission: ', request.body)
-    response.send('Thank you for getting in touch with us!')
+    response.send('<h2>Message sent!</h2><p>Thank you for getting in touch with us!</p>')
 })
 
 app.get('/about', (request, response) => {
-    response.send('You can find all sorts of delicacies in our shop. Stay tuned for new items on our menu!')
+    response.send('<h1>About</h1><p>You can find all sorts of delicacies in our shop. Stay tuned for new items on our menu!</p>')
 })
 
 app.get('/search', (request, response) => {
@@ -33,24 +33,31 @@ app.get('/search', (request, response) => {
 app.get('/hello', (request, response) => {
     const name = 'Joe'
     const fakeVisitorNumber = '340'
-    response.send(`Hello ${name}! You are visitor number ${fakeVisitorNumber}`)
+    response.send(`<h1>Hello ${name}!</h1> <p>You are visitor number ${fakeVisitorNumber}</p>`)
 })
 
 app.get('/calculate', (request, response) => {
     let num1 = 23
     let num2 = 45
     const average = (num1 + num2) / 2
-    response.send(`The average of number ${num1} and ${num2} is ${average}.`)
+    response.send(`<h3>The average of number ${num1} and ${num2} is ${average}.</h3>`)
 })
 
 app.get('/cookies', (request,response) => {
     console.log(request.query)
-    response.send('Great cookies here!')
+    response.send(`
+        <h1>Cookies</h1>
+        <p>Here you will find all the cookies!"</p>
+        <ul>
+            <li>Chocolate Chip</li>
+            <li>Banana</li>
+        </ul>
+    `)
 })
 
 app.get('/cookies/:slug', (request, response) => {
     const cookiesId = request.params.slug
-    response.send(`You chose the cookie with the ID ${cookiesId}.`)
+    response.send(`<h3>You chose the cookie with the ID ${cookiesId}.</h3>`)
 })
 
 app.listen(PORT , () => {
