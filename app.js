150e62abfcc48d49c0a20a605e34571016f57d26
Till1983 <till_ermold@web.de>
2024-05-30 15:00:05 +0200

    ADD ejs

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import { logger } from './middlewares/logger.js'
 const app = express()
 const PORT = 3000
 
+app.set('view engine', 'ejs')
+
 app.use(logger)
 app.use('/assets', express.static('public'))
 app.use(express.urlencoded({extended: true}))
