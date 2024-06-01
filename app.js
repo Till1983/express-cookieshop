a68fb58c3bb2ce712433f7d3185f13c90e233dd1
Till1983 <till_ermold@web.de>
2024-06-01 20:33:36 +0200

    ADD: dotenv, mongoose, database connection

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import express, { request, response } from 'express'
 import { logger } from './middlewares/logger.js'
 import { readablePrice } from './helpers/cookie-views.js'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
+const DATABASE_URL = process.env.DATABASE_URL
+
+mongoose.connect(DATABASE_URL)
+    .then(() => console.log('Database connected'))
+    .catch(error => console.log(error))
 
 app.set('view engine', 'ejs')
 
